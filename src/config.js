81714c26b315601558681ee042c8493f230730bb
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-01-14 00:51:13 +0000

    ffix: use port from env variable

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = {
   name,
-  port: 8080,
+  port: process.env.PORT || 8080,
   mongo: {
     maxLimit: 100,
     connectionString: process.env.MONGO_CONNECTION_STRING,
