5a1e4b67347c2d8b6ebf866cc978acb1af01e737
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-01-14 01:26:41 +0000

    add loggin

--- src/handlers/token.js
+++ src/handlers/token.js
@@ -1,7 +1,10 @@
 var jwt = require('jsonwebtoken');
+const logger = require('pino')();
 
 async function generateToken(req, res) {
-  const clientId = req.header('x-client-id'); // <<
+  logger.info({ msg: `Generating token` })
+  const clientId = req.headers['x-client-id'];
+  logger.info({ msg: `Generating token for clientId ${clientId}` })
   const authToken = jwt.sign(
     { clientId },
     'secret',
@@ -10,6 +13,7 @@ async function generateToken(req, res) {
       // algorithm: 'RS256'
     }
   );
+  logger.info({ msg: `authToken generated` })
   res.setHeader('Set-Cookie', [`authToken=${authToken}; HttpOnly`]);
   res.status(200).json({ authToken })
 }
