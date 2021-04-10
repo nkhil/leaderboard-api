412b22de229e2a5f15cc4a82be783cee1dd60b9d
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-04-10 14:06:32 +0100

    feat: add secret helper library

--- src/lib/apikey/index.js
+++ src/lib/apikey/index.js
@@ -1,45 +1,25 @@
-const crypto = require('crypto');
+const secretHelper = require('@subgeniuscorp/secret-helper');
 const { SALT_LENGTH } = require('../../constants');
 
-function generateSalt(len) {
-  const set = '0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ';
-  const setLen = set.length;
-  let salt = '';
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < len; i++) {
-    const p = Math.floor(Math.random() * setLen);
-    salt += set[p];
-  }
-  return salt;
-}
+const sh = secretHelper({ saltLength: SALT_LENGTH });
 
-function md5(string) {
-  return crypto.createHash('md5').update(string).digest('hex');
+function generateSalt() {
+  return sh.generateSalt();
 }
 
 function createHash(str) {
-  const salt = generateSalt(SALT_LENGTH);
-  const hash = md5(str + salt);
-  return salt + hash;
+  return sh.createHash(str);
 }
 
 function validateHash(hash, valueFromRequest) {
-  const salt = hash.substr(0, SALT_LENGTH);
-  const validHash = salt + md5(valueFromRequest + salt);
-  return hash === validHash;
+  return sh.validateHash({
+    hashValue: hash,
+    valueToCompare: valueFromRequest,
+  });
 }
 
-function generateApiKey(len) {
-  if (!len) throw new Error('API Key length not defined.');
-  return crypto.randomBytes(64).toString('hex').slice(0, len);
-}
-
-function createClientId(length) {
-  return 'leaderboard_client_'.concat(generateSalt(length));
-}
-
-function createClientSecret(length) {
-  return generateSalt(length);
+function generateApiKey() {
+  return sh.generateApiKey();
 }
 
 module.exports = {
@@ -47,6 +27,4 @@ module.exports = {
   createHash,
   validateHash,
   generateSalt,
-  createClientId,
-  createClientSecret,
 };
