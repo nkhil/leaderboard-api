0077b1748eb553eec34e76f382ec3a8d7b31f08e
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-04-17 02:29:30 +0100

    chore: modify test script

--- test.sh
+++ test.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-END='\033[0m'
-
-function checkError() {
-    if [ $? -eq "1" ]; then
-        echo " ";
-        echo -e "${RED} Tests exited with exit code 1${END}";
-        exit 1;
-    fi
-}
-
-docker-compose up --build test
-checkError $?
+docker-compose up --build --exit-code-from test
